b1b9e68ff17b71748602f8c73aaa2339a05d1d7b
Clem <gc117711@ohio.edu>
2018-05-29 21:58:36 -0400

    validated gvf guidance class with matlab

--- cfControl/testingControlClass.py
+++ cfControl/testingControlClass.py
@@ -1,7 +1,23 @@
 from cfControlClass import cfControlClass
-
+from matplotlib import pyplot as plt
 import threading
 import time
+from vectorField import vectorField as vf
+
+velocity = 0.25
+# Determine these values from MATLAB
+m = 1
+y_ratio = 0.5
+k = 2.0
+Ho = -3.3
+theta_r = velocity / 0.35
+
+VF = vf(m, y_ratio, k, Ho, theta_r, velocity)
+VF.calcFullField()
+
+VF.simulateDubins(velocity)
+plt.pause(1)
+
 
 uav = cfControlClass('CF_1',(True,'TestingVF'),True)
 time.sleep(2)
@@ -9,26 +25,6 @@ while uav.active:
 
     uav.takeoff(1)
     time.sleep(5)
-
-
-
-
-
-    uav.goto(1,0,1)
-    time.sleep(6)
-    # print("Starting VF Guidance")
-    # uav.startVFGuidanceManager()
-    # time.sleep(5)
-
-    time.sleep(6)
-
-    uav.goto(-1,0,1)
-    time.sleep(5)
-    uav.goto(0,0,1)
-    time.sleep(6)
-
-
-
     uav.land()
     time.sleep(2)
 
